a2452788758cc733a6ec53367ec674dc6e10f0e2
dayrow90 <dayrow90@github.com>
2026-03-08 00:43:10 +0800

    fix

--- clean.bat
+++ clean.bat
@@ -1,25 +1,25 @@
 @echo off
 REM Clean Script for Video Enhancement Frames
-REM This script directly deletes contents of tmp_frames and out_frames folders
+REM This script directly deletes contents of output\frames_extract and output\frames_enhance folders
 
 title Cleaning Frames
 
-echo Cleaning tmp_frames folder...
-if exist "tmp_frames\" (
-    del /q "tmp_frames\*" >nul 2>&1
-    for /d %%i in ("tmp_frames\*") do rmdir /q "%%i" >nul 2>&1
+echo Cleaning output\frames_extract folder...
+if exist "output\frames_extract\" (
+    del /q "output\frames_extract\*" >nul 2>&1
+    for /d %%i in ("output\frames_extract\*") do rmdir /q "%%i" >nul 2>&1
     echo Done.
 ) else (
-    echo tmp_frames folder does not exist.
+    echo output\frames_extract folder does not exist.
 )
 
-echo Cleaning out_frames folder...
-if exist "out_frames\" (
-    del /q "out_frames\*" >nul 2>&1
-    for /d %%i in ("out_frames\*") do rmdir /q "%%i" >nul 2>&1
+echo Cleaning output\frames_enhance folder...
+if exist "output\frames_enhance\" (
+    del /q "output\frames_enhance\*" >nul 2>&1
+    for /d %%i in ("output\frames_enhance\*") do rmdir /q "%%i" >nul 2>&1
     echo Done.
 ) else (
-    echo out_frames folder does not exist.
+    echo output\frames_enhance folder does not exist.
 )
 
 echo.
